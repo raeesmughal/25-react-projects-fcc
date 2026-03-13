22761505a103ca0f612887d1fbd25b6ea689692d
Raees Mughal <raees.mughal.official@gmail.com>
2026-03-13 17:59:34 +0500

    coderabbit suggesstions updated

--- src/components/qr-code-generator/index.jsx
+++ src/components/qr-code-generator/index.jsx
@@ -14,15 +14,25 @@ export default function QrCodeGenerator() {
         <div className="qr-container">
             <h1>QR Code Generator</h1>
             <div>
-                <input onChange={(e) => setInput(e.target.value)} type="text" name="qr-code" placeholder="Enter Your Value Here" value={input}/>
+                <label htmlFor="qr-code-input">Value to encode : </label>
+                <input
+                    id="qr-code-input"
+                    onChange={(e) => setInput(e.target.value)}
+                    type="text"
+                    name="qr-code"
+                    placeholder="Enter Your Value Here"
+                    value={input}
+                />
                 <button type="button" onClick={() => handleGenerateQrCode()} disabled={input && input.trim() !== '' ? false : true}>Generate</button>
             </div>
-            <div>
+
+            <div style={{ height: "auto", padding: "3rem", maxWidth: "500px", width: "100%" }}>
                 <QRCode
                     id="qr-code-value"
                     value={qrCode}
-                    size={400}
-                    style={{ backgroundColor: '#fff' }}
+                    // size={256}
+                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                // viewBox="0 0 256 256"
                 />
             </div>
         </div>
