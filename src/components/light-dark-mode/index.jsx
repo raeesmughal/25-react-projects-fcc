37dbdbf99dd2bc2022983977c3d1163551f26da7
Raees Mughal <raees.mughal.official@gmail.com>
2026-03-20 11:34:59 +0500

    coderabbit changed updated

--- src/components/light-dark-mode/index.jsx
+++ src/components/light-dark-mode/index.jsx
@@ -7,8 +7,6 @@ export default function LightDarkMode() {
         setTheme(theme === 'light' ? 'dark' : 'light')
     }
 
-    console.log(theme)
-
     return <div className="light-dark-mode" data-theme={theme}>
         <div className="container">
             <p>Hello World! <br /><br /> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt veritatis quibusdam, libero nisi voluptas expedita, culpa suscipit, voluptatibus distinctio asperiores sint accusamus est optio! Ad quibusdam blanditiis, libero similique veritatis praesentium autem sapiente iure aspernatur quisquam? Atque quibusdam quod vel dolore magni molestiae optio enim assumenda, praesentium non dolor eligendi.</p>
