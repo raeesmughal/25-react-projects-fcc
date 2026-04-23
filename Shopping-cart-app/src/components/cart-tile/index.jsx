56ead1c324a9e3832f23776aa6101edbaad36efb
Raees Mughal <raees.mughal.official@gmail.com>
2026-04-23 19:46:34 +0500

    linting issue/warning resolved

--- Shopping-cart-app/src/components/cart-tile/index.jsx
+++ Shopping-cart-app/src/components/cart-tile/index.jsx
@@ -12,7 +12,7 @@ export default function CartTile({ cartItem }) {
 
     return <>
         <div className="flex items-center p-5 justify-between bg-red-500 mt-2 mb-2 rounded-xl flex-col">
-            <div className="flex p-3 max-w-[400px] truncate">
+            <div className="flex p-3 max-w-100 truncate">
                 <img src={cartItem?.image} alt={cartItem?.title} className="h-28 rounded-lg" />
                 <div className="ml-10 self-start space-y-5 ">
                     <h1 className="text-xl text-white font-bold  truncate">{cartItem?.title}</h1>
--- Shopping-cart-app/src/components/product-tile/index.jsx
+++ Shopping-cart-app/src/components/product-tile/index.jsx
@@ -20,8 +20,8 @@ export default function ProductTile({ product }) {
 
 
     return <div>
-        <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl ">
-            <div className="h-[180px]">
+        <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-90 mt-10 ml-5 rounded-xl ">
+            <div className="h-45">
                 <img
                     src={product?.image}
                     alt={product?.title}
